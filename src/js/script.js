a4235cdcad65c8d0361d510c0cad9bf1010ea2b4
Aleksey <leshaganzha31@gmail.com>
2025-12-26 18:40:28 +0900

    Работал над производительностью сайта

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,51 @@
 import 'nodelist-foreach-polyfill';
 import '../css/style.css';
-
-import tabs from './modules/tabs.js';
-import modal from './modules/modal.js';
-import timer from './modules/timer.js';
-import cards from './modules/cards.js';
-import forms from './modules/forms.js';
-import slider from './modules/slider.js';
-import calc from './modules/calculator.js';
 import { openModal } from './modules/modal.js';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
-
-  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-  modal('[data-modal]', '.modal', modalTimerId);
-  timer('.timer', '2020-06-11');
-  cards();
-  forms('form', modalTimerId);
-  slider({
-    container: '.offer__slider',
-    nextArrow: '.offer__slider-next',
-    prevArrow: '.offer__slider-prev',
-    slide: '.offer__slide',
-    totalCounter: '#total',
-    currentCounter: '#current',
-    wrapper: '.offer__slider-wrapper',
-    field: '.offer__slider-inner',
+  const modalTimerId = setTimeout(() => {
+    openModal('.modal', modalTimerId);
+  }, 50000);
+  import('./modules/timer.js').then(({ default: timer }) => {
+    timer('.timer', '2020-06-11');
   });
-  calc();
+
+  import('./modules/modal.js').then(({ default: modal }) => {
+    const modalTimerId = setTimeout(() => {
+      import('./modules/modal.js').then(({ openModal }) => {
+        openModal('.modal', modalTimerId);
+      });
+    }, 50000);
+
+    modal('[data-modal]', '.modal', modalTimerId);
+  });
+
+  import('./modules/tabs.js').then(({ default: tabs }) => {
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+  });
+
+  import('./modules/cards.js').then(({ default: cards }) => {
+    cards();
+  });
+
+  import('./modules/forms.js').then(({ default: forms }) => {
+    forms('form', modalTimerId);
+  });
+
+  import('./modules/slider.js').then(({ default: slider }) => {
+    slider({
+      container: '.offer__slider',
+      nextArrow: '.offer__slider-next',
+      prevArrow: '.offer__slider-prev',
+      slide: '.offer__slide',
+      totalCounter: '#total',
+      currentCounter: '#current',
+      wrapper: '.offer__slider-wrapper',
+      field: '.offer__slider-inner',
+    });
+  });
+
+  import('./modules/calculator.js').then(({ default: calc }) => {
+    calc();
+  });
 });
